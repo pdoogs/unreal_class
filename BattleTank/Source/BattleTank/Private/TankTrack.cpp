f296b46bb26e5c37b49f153158ce87adaf5e6902
pdugan <patrick.c.dugan@gmail.com>
2017-12-09 15:01:10 -0500

    BT 182.0 Programmatic Sideways Friction - Tank Track Ticks

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,6 +2,16 @@
 
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	UE_LOG(LogTemp, Warning, TEXT("TICKING"));
+}
+
 void UTankTrack::SetThrottle(float Throttle)
 {
 	// TODO Clamp actual Throttle value so Player can't speed up tank
